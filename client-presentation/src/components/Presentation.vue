51e6c69d15c19eb5fb33a29d92b4242d9d24c25d
Michal Michalczuk <michalczukm@gmail.com>
2018-01-23 00:06:24 +0100

    slide transform effect on presentation

--- client-presentation/src/components/Presentation.vue
+++ client-presentation/src/components/Presentation.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        <feature v-bind:feature="currentFeature"></feature>
+        <transition name="slide-fade">
+            <feature class="x" v-bind:feature="currentFeature" v-bind:class="{'slide-out': !transition, 'slide-in': transition }"></feature>
+        </transition>
     </div>
 </template>
 
@@ -22,10 +24,31 @@ export default {
             required: true
         }
     },
+    data: function () {
+        return { transition: false };
+    },
     computed: {
         currentFeature: function () {
             return this.features.find(feature => feature.id === this.slideFeatureId);
         }
+    },
+    watch: {
+        slideFeatureId: function (value, oldValue) {
+            this.transition = !this.transition;
+            setTimeout(() => { this.transition = !this.transition; }, 500);
+        }
     }
 };
 </script>
+<style lang="scss" scoped>
+.slide-out {
+  transition: 0.5s;
+  transform: translate(0%, 0);
+}
+.slide-in {
+  transition: 0.5s;
+  opacity: 0;
+  transform: translate(100%, 0);
+}
+</style>
+
