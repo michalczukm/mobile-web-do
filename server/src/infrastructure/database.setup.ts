fd495883aa69c2990801eea4c1a9c075fc626bc4
Michał Michalczuk <michalczukm@gmail.com>
2018-08-22 02:35:18 +0200

    use new mongo url parser

--- server/src/infrastructure/database.setup.ts
+++ server/src/infrastructure/database.setup.ts
@@ -1,12 +1,16 @@
 import * as Mongoose from 'mongoose';
 
+const defaultConnectionOptions = {
+    useNewUrlParser: true
+};
+
 export const databaseSetup = {
     init: (config: { connectionString: string }, options?: {}): Promise<any> => {
         Mongoose.connection
             .on('error', () => console.log(`### Error: Unable to connect to database: ${config.connectionString}`))
             .once('open', () => console.log(`=== Connected to database: ${config.connectionString}`));
 
-        return Mongoose.connect(config.connectionString, options);
+        return Mongoose.connect(config.connectionString, { ...defaultConnectionOptions, ...options });
     },
     dispose: () => {
         Mongoose.disconnect();
